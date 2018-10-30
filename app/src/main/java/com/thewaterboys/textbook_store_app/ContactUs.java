cc7035dcb3840d61f84a3cd4f980592028f043e0
Nicholas Meier <nlm15@my.fsu.edu>
2018-10-30 11:07:34 -0400

    Added ContactUs and AboutUs Activities - Nicholas

---
+++ app/src/main/java/com/thewaterboys/textbook_store_app/ContactUs.java
@@ -0,0 +1,21 @@
+package com.thewaterboys.textbook_store_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+public class ContactUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void yourMethodName(View v){
+        startActivity(new Intent(ContactUs.this, AboutUs.class));
+    }
+
+}
