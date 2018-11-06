d1943cc93ed32867b7b5fe2e203ada63deb6b20f
Nicholas Meier <nlm15@my.fsu.edu>
2018-11-06 09:19:02 -0500

    Testing writing to firebase database

--- app/src/main/java/com/thewaterboys/textbook_store_app/MainActivity.java
+++ app/src/main/java/com/thewaterboys/textbook_store_app/MainActivity.java
@@ -8,26 +8,32 @@ import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnViewDatabase;
 
-    private DatabaseReference mDatabase;
+/*    private DatabaseReference mDatabase;
 
     private void writeNewBook(String Title, String Author, String ISBN) {
         Books book = new Books(Title, Author, ISBN);
 
         mDatabase.child("Books").child(Title).setValue(book);
-    }
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        writeNewBook("Harry Potter", "J.K. Rowling", "123456789");
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Books newBook = new Books("Harry Potter", "JK", "123456789");
+        ApiFuture<WriteResult> future = db.collection("Books").document("JK").set(newBook);
+
+       /* mDatabase = FirebaseDatabase.getInstance().getReference();
+        writeNewBook("Harry Potter", "J.K. Rowling", "123456789");*/
 
 
 
