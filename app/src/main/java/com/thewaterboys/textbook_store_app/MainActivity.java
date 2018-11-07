84ec6bb4a9f422ee738dde29d3625316c43f8ed3
Nicholas Meier <nlm15@my.fsu.edu>
2018-11-07 12:20:00 -0500

    Added Firebase Authentication and Updated Dependencies for Proper Compilation

--- app/src/main/java/com/thewaterboys/textbook_store_app/MainActivity.java
+++ app/src/main/java/com/thewaterboys/textbook_store_app/MainActivity.java
@@ -2,11 +2,20 @@ package com.thewaterboys.textbook_store_app;
 
 import android.content.Intent;
 import android.print.PrintDocumentAdapter;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -14,6 +23,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class MainActivity extends AppCompatActivity {
 
     private Button btnViewDatabase;
+
+    private void updateUI(FirebaseUser user) {}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +45,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
     }
 }
