3cebb65b2b31bcc43eb1cbd1001f7afa54d8c67c
TheWaterboysTeam <43378458+TheWaterboysTeam@users.noreply.github.com>
2018-11-15 13:14:24 -0500

    reverted file back to nicks commit
    reverted file back to nicks commit

--- app/src/main/java/com/thewaterboys/textbook_store_app/AccountCreated.java
+++ app/src/main/java/com/thewaterboys/textbook_store_app/AccountCreated.java
@@ -1,15 +1,24 @@
 package com.thewaterboys.textbook_store_app;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AccountCreated extends AppCompatActivity {
 
-    Button list_book;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "AccountCreated";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +26,10 @@ public class AccountCreated extends AppCompatActivity {
         setContentView(R.layout.activity_account_created);
 
 
-        EditText FirstNameOut = findViewById(R.id.firstName);
-        EditText LastNameOut = findViewById(R.id.lastName);
-        EditText emailOut = findViewById(R.id.email);
-        EditText phoneOut = findViewById(R.id.phone);
+        final EditText FirstNameOut = findViewById(R.id.firstName);
+        final EditText LastNameOut = findViewById(R.id.lastName);
+        final EditText emailOut = findViewById(R.id.email);
+        final EditText phoneOut = findViewById(R.id.phone);
 
         Intent myIntent = getIntent();
 
@@ -31,21 +40,25 @@ public class AccountCreated extends AppCompatActivity {
         emailOut.setText(mybundle.getString("EmailOut"));
         phoneOut.setText(mybundle.getString("PhoneOut"));
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Users newUser = new Users(mybundle.getString("FirstNameOut"), mybundle.getString("LastNameOut"), mybundle.getString("EmailOut"), mybundle.getString("PhoneOut"));
+        db.collection("Users").add(newUser);
 
-        /*list_book.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                listBook();
-            }
-        });*/
+        //FOLLOWING CODE IS FOR THE PURPOSE OF TESTING FIREBASE AUTHENTICATION
+        //AND THE SIGNING UP OF NEW USERS
+        mAuth = FirebaseAuth.getInstance();
 
-
+        mAuth.createUserWithEmailAndPassword(mybundle.getString("EmailOut"), mybundle.getString("PasswordOut"))
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            }
+                    }
+                     });
+                //THIS COMMENT MARKS THE END OF THE PORTION OF CODE FOR TESTING FIREBASE AUTH
     }
-
-    /*void listBook() {
-
-        Intent MyIntent = new Intent (AccountCreated.this, CreateListingActivity.class);
-
-        startActivity(MyIntent);
-    }*/
 }
