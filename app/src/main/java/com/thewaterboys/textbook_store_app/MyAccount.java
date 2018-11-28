8e9c27271ef24e8aa7911fb260a229e5477b9a78
Nicholas Meier <nlm15@my.fsu.edu>
2018-11-27 21:32:29 -0500

    MyAccount page now sends verification email (just for testing, will be moved to the create account page), and also allows a user to delete their account.

--- app/src/main/java/com/thewaterboys/textbook_store_app/MyAccount.java
+++ app/src/main/java/com/thewaterboys/textbook_store_app/MyAccount.java
@@ -1,15 +1,23 @@
 package com.thewaterboys.textbook_store_app;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MyAccount extends AppCompatActivity {
+
+    final static String TAG = "MyAccount";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +37,42 @@ public class MyAccount extends AppCompatActivity {
             // The user's ID, unique to the Firebase project. Do NOT use this value to
             // authenticate with your backend server, if you have one. Use
             // FirebaseUser.getIdToken() instead.
-            /*String uid = user.getUid();
-            Toast.makeText(MyAccount.this, email, Toast.LENGTH_SHORT).show();
-            System.out.print(email);*/
+            //String uid = user.getUid();
 
             EditText Email = (EditText) findViewById(R.id.email);
             Email.setText(email);
+
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+
+            user.sendEmailVerification()
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "Email sent.");
+                            }
+                        }
+                    });
         }
+
+        Button DeleteAccountButton = (Button) findViewById(R.id.delAccBtn);
+
+        DeleteAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                user.delete()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "User account deleted.");
+                                }
+                            }
+                        });
+            }
+        });
 
     }
 }
