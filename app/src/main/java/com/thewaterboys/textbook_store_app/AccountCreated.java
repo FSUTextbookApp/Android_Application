aa593ee9312d0bbfe61858bc0c45ff5e4d41fc88
Nicholas Meier <nlm15@my.fsu.edu>
2018-11-28 15:42:08 -0500

    Fixed an issue that I created when adding user sign in. Everything works correctly now.

--- app/src/main/java/com/thewaterboys/textbook_store_app/AccountCreated.java
+++ app/src/main/java/com/thewaterboys/textbook_store_app/AccountCreated.java
@@ -35,32 +35,10 @@ public class AccountCreated extends AppCompatActivity {
         Bundle mybundle = myIntent.getExtras();
 
         if (mybundle != null) {
-
             FirstNameOut.setText(mybundle.getString("FirstNameOut"));
             LastNameOut.setText(mybundle.getString("LastNameOut"));
             emailOut.setText(mybundle.getString("EmailOut"));
             phoneOut.setText(mybundle.getString("PhoneOut"));
-
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-            Users newUser = new Users(mybundle.getString("FirstNameOut"), mybundle.getString("LastNameOut"), mybundle.getString("EmailOut"), mybundle.getString("PhoneOut"));
-            db.collection("Users").add(newUser);
-
-            //FOLLOWING CODE IS FOR THE PURPOSE OF TESTING FIREBASE AUTHENTICATION
-            //AND THE SIGNING UP OF NEW USERS
-            mAuth = FirebaseAuth.getInstance();
-
-            mAuth.createUserWithEmailAndPassword(mybundle.getString("EmailOut"), mybundle.getString("PasswordOut"))
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                Log.d(TAG, "createUserWithEmail:success");
-                                FirebaseUser user = mAuth.getCurrentUser();
-                            }
-                        }
-                    });
-            //THIS COMMENT MARKS THE END OF THE PORTION OF CODE FOR TESTING FIREBASE AUTH
         }
     }
 }
