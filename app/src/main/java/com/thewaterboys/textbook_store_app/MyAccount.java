11e64b81ec32e46050f6e2914e80d9d4212d2fe1
Nicholas Meier <nlm15@my.fsu.edu>
2018-12-02 14:54:18 -0500

    Fixed issue with displaying proper book information in the recycler view.

--- app/src/main/java/com/thewaterboys/textbook_store_app/MyAccount.java
+++ app/src/main/java/com/thewaterboys/textbook_store_app/MyAccount.java
@@ -146,8 +146,15 @@ public class MyAccount extends AppCompatActivity {
                         // Add code here to update the UI based on the item selected
                         // For example, swap UI fragments here
 
-                        Intent intent = new Intent(MyAccount.this, AboutUs.class);
-                        startActivity(intent);
+                        if(menuItem.getTitle().equals("Home Page")) {
+                            Intent intent = new Intent(MyAccount.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+
+                        if(menuItem.getTitle().equals("About Us")) {
+                            Intent intent = new Intent(MyAccount.this, AboutUs.class);
+                            startActivity(intent);
+                        }
 
                         return true;
                     }
