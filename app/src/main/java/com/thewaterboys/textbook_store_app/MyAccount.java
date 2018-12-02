d3acd4f13abaf77333e1c473551297f5deada147
marcobarnes <marcobarnes@gmail.com>
2018-12-02 13:06:10 -0500

    added recycler
    fsdsfds

--- app/src/main/java/com/thewaterboys/textbook_store_app/MyAccount.java
+++ app/src/main/java/com/thewaterboys/textbook_store_app/MyAccount.java
@@ -4,20 +4,34 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class MyAccount extends AppCompatActivity {
 
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference BookRef = db.collection("Books");
+    private NoteAdapter adapter;
+    private Query query;
+
     final static String TAG = "MyAccount";
+    String name;
+    String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +42,8 @@ public class MyAccount extends AppCompatActivity {
 
         if (user != null) {
             // Name, email address, and profile photo Url
-            String name = user.getDisplayName();
-            String email = user.getEmail();
+            name = user.getDisplayName();
+            email = user.getEmail();
 
             // Check if user's email is verified
             boolean emailVerified = user.isEmailVerified();
@@ -105,5 +119,48 @@ public class MyAccount extends AppCompatActivity {
             }
         });
 
+        setUpRecyclerView();
+
+    }
+
+    private void setUpRecyclerView() {
+        //Query query = notebookRef.orderBy("priority", Query.Direction.DESCENDING);
+        query = BookRef.whereEqualTo("sellersEmail", email);
+
+        FirestoreRecyclerOptions<Note> options = new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query, Note.class)
+                .build();
+
+        adapter = new NoteAdapter(options);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view_user_books);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                adapter.deleteItem(viewHolder.getAdapterPosition());
+            }
+        }).attachToRecyclerView(recyclerView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
